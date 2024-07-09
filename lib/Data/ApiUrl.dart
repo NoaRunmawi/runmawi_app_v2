f8c3e76aea88f429242fda4c4b02f782f821d6d1
unknown <runmawiproduction@gmail.com>
2024-07-09 12:45:29 +0530

    Version-0.1

---
+++ lib/Data/ApiUrl.dart
@@ -0,0 +1,9 @@
+class AppUrl {
+  static var baseUrl = "http://148.113.0.158/mobile_api/route.php" ;
+  static var refundPolicy = "https://runmawi.com/page.php?id=paymentpolicies";
+  static var termsCondition = "https://runmawi.com/page.php?id=termsofuse";
+  static var privacyPolicy = "https://runmawi.com/page.php?id=privacypolicy";
+  static var aboutUs = "https://runmawi.com/page.php?id=aboutus";
+  static var contactUs = "https://runmawi.com/page.php?id=contactus";
+  static var Faq = "https://runmawi.com/page.php?id=faq";
+}
